e9fd0bee7ab19bd600ec2cbe8ddfeca79149481d
T-J-Liza <tahminajannat43@gmail.com>
2025-10-13 18:07:42 +0600

    first commit

---
+++ Assignment-1/Assignment_1.dart
@@ -0,0 +1,26 @@
+// print your name
+/*
+void main(){
+  print('liza');
+}
+
+*/
+
+//print Hello I am "Jhon Doe"and Hello I\'am 'Jhone doe' with single/double quotes
+/*void main() {
+  print('Hello I am "John Doe"');       // double quotes inside single-quoted string
+  print("Hello I'am 'John Doe'");       // single quotes inside double-quoted string
+  // or using escaping:
+  print("Hello I am \"John Doe\"");
+  print('Hello I\'am \'John Doe\'');
+}*/
+
+//Declare constant type int set value 7
+
+/*void main() {
+  const int myConstant = 7;
+  print('myConstant = $myConstant');   //$ print the value of myConstant
+}
+*/
+
+//write a program in dart thatt find simple interest .Formula = (p*t*r)/100
