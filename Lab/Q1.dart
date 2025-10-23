31d638beff16602966363ce7c40eacad7bf1ec67
T-J-Liza <tahminajannat43@gmail.com>
2025-10-24 00:11:38 +0600

    Assignment3 code folder committed!!

---
+++ Lab/Q1.dart
@@ -0,0 +1,29 @@
+/*class Student{
+  String? name;
+  int? id;
+  double? marks; 
+  void displayDetails(){
+  print("name of the student: $name.");
+  print("id of the student: $id.");
+  print("marks of the student: $marks.");
+
+}
+}
+void main(){
+  Student obj = new Student();
+  obj.id = 172;
+  obj.name ="liza";
+  obj.marks = 55.5;
+  obj.displayDetails();
+}*/
+
+/*class car{
+  String? brand;
+  String? model;
+  int? year;
+
+  car(String brand,String model,int year){
+    this.brand = brand;
+    this.model = 
+  }
+}*/
--- Lab/functioncall.dart
+++ Lab/functioncall.dart
@@ -20,3 +20,61 @@ void main(){
     return 18;
   }
 }*/
+
+/*int add(int a,int b){
+  int sum = a+b;
+  return sum;
+}
+void main(){
+  int num1 = 10;
+  int num2 = 20;
+  int total = add(num1,num2);
+  print("the sum is $total");
+}*/
+
+/*void add(int a,int b){
+  var total;
+  var total = a+b;
+  return total;
+}
+void mul(int a,int b){
+  var total = a*b;
+  print("multiplication is $total");
+}
+String greet(){
+  String greet = "Welcome";
+  return greet;
+}
+void greetings(){
+  print("hello world!!");
+}
+void main(){
+  var total = add(2,3);
+  print("total sum $total");
+  mul(2,3);
+  var greeting = greet();
+  print("Greeting: $greeting");
+  greetings();
+}*/
+
+/*void printInfo(String name,String gender,[String title = "Sir/ma'am"]){
+  print("Hello $title $name your gender is $gender");
+
+}
+void main(){
+  printInfo("john","male");
+  printInfo("john","male","Mr.");
+  printInfo("kavya","female","Ms.");
+}*/
+
+/*void add(int a,int b,[int c = 0]){
+    int sum = 0;
+    sum = a+b+c;
+    print("the sum is $sum");
+}
+
+void main(){
+    add(10,20);
+    add(10,20,30);
+   
+}*/
